a30e6d13cb1212aeb2894482e76b53b9f471aedf
Matthew Casperson <matthewcasperson@gmail.com>
2019-01-21 14:01:08 +1000

    Removed unwanted files from the module

--- Publish.ps1
+++ Publish.ps1
@@ -1,6 +1,17 @@
 $PublishParams = @{
-    Path = 'C:\Users\Matthew\Development\PowershellRetry'
+    Path = "$(Get-Location)\PowershellRetry"
     NuGetApiKey = $env:PowershellGalleryApiKey
 }
 
+# There is no way to exclude files, so move the files were are interested in to a temporary location
+mkdir PowershellRetry
+mkdir PowershellRetry/Public
+cp PowershellRetry.psd1 PowershellRetry
+cp PowershellRetry.psm1 PowershellRetry
+cp Public/Retry.ps1 PowershellRetry/Public
+
+# Publish the module
 Publish-Module @PublishParams
+
+# Clear the temporary files
+rm -Recurse PowershellRetry
