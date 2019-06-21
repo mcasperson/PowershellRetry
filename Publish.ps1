92ef7ad3047b3fb16950ec05bb07123fe4d6ac09
mcasperson <matthewcasperson@gmail.com>
2019-06-21 13:48:07 +1000

    sigh

--- Publish.ps1
+++ Publish.ps1
@@ -3,8 +3,8 @@ param (
     [string]$Path = "$(Get-Location)\PowershellRetry"
 )
 
-Install-PackageProvider Nuget –Force -Scope CurrentUser
-Install-Module –Name PowerShellGet –Force -Scope CurrentUser
+Install-PackageProvider Nuget -Force -Scope CurrentUser
+Install-Module –Name PowerShellGet -Force -Scope CurrentUser
 
 # There is no way to exclude files, so move the files were are interested in to a temporary location
 If (Test-Path PowershellRetry){
